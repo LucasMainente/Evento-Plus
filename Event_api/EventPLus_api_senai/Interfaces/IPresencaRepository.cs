286e2d4c808cc06fb25f127ad2651289d84ad1cf
Lucas <lucas.m.ferreira16@aluno.senai.br>
2025-03-25 10:00:38 -0300

    subindo git

---
+++ Event_api/EventPLus_api_senai/Interfaces/IPresencaRepository.cs
@@ -0,0 +1,14 @@
+﻿using Eventplus_api_senai.Domais;
+
+namespace Eventplus_api_senai.Interfaces
+{
+    public interface IPresencaRepository
+    {
+        List<Presenca> Listar();
+        Presenca BuscarPorId(Guid id);
+        void Atualizar(Guid id, Presenca presenca);
+        void Inscrever(Presenca Inscricao);
+        List<Presenca> ListarMinhas(Guid id);
+        void Deletar(Guid id);
+    }
+}
